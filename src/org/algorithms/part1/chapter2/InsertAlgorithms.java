e93f6e0e0de12671688a0df842fe944bf1536eed
Ryan zyz <zyzcharry@163.com>
2017-12-22 09:19:52 +0800

    代码提交

--- src/org/algorithms/part1/chapter2/InsertAlgorithms.java
+++ src/org/algorithms/part1/chapter2/InsertAlgorithms.java
@@ -1,5 +1,8 @@
 package org.algorithms.part1.chapter2;
 
+/**
+ * 插入排序的时间复杂度位n2
+ */
 public class InsertAlgorithms {
     public static void main(String[] args) {
         int[] arr = new int[]{4,6,7,1,2,3};
@@ -7,10 +10,16 @@ public class InsertAlgorithms {
         for (int n : arr) {
             System.out.print(n + "\t");
         }
+
+        System.out.println();
+        insertSortDesc(arr);
+        for (int n : arr) {
+            System.out.print(n + "\t");
+        }
     }
 
     /**
-     * 插入排序
+     * 插入排序 升序排序
      * @param arr
      */
     public static void insertSort(int[] arr) {
@@ -19,6 +28,23 @@ public class InsertAlgorithms {
             target = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > target) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = target;
+        }
+    }
+
+    /**
+     * 插入排序 升序排序
+     * @param arr
+     */
+    public static void insertSortDesc(int[] arr) {
+        int target;
+        for (int i = 1; i < arr.length; i++) {
+            target = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] < target) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
